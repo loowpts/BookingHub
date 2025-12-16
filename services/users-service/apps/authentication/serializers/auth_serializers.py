ad2c9c18c9a28395b8464ca7bfe28a14d958df4e
loowpts <pavelbariik@yandex.ru>
2025-12-16 15:09:21 +0300

    -- Added (UserLoginSerializer)

--- services/users-service/apps/authentication/serializers/auth_serializers.py
+++ services/users-service/apps/authentication/serializers/auth_serializers.py
@@ -1,10 +1,14 @@
+import jwt
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.hashers import check_password
+from apps.authentication.models.refresh_token import RefreshToken
+from django.conf import settings
 from django.core import exceptions
 from models.user import User
 
-
-class UserRegistrationSerializer(serializers.ModelSerializer):
+        
+class UserRegistrationSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True, min_length=8)
     password_confirm = serializers.CharField(write_only=True)
     
@@ -53,3 +57,69 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         # UserProfile.object.create(user=user)
         
         return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        try:
+            user = User.objects.filter(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError('Invalid credentials')
+        
+        if not user.check_password(password):
+            raise serializers.ValidationError('Invalid credentials')
+
+        if not user.is_active:
+            raise serializers.ValidationError('Account is inactive')
+        
+        if not user.is_email_verified:
+            raise serializers.ValidationError('Email not verified')
+        
+        attrs['user'] = user
+        
+        return attrs
+    
+    
+class TokenRefreshSerializer(serializers.Serializer):
+    
+    def validate(self, attrs):
+        request = self.context.get('request')
+        if not request:
+            raise serializers.ValidationError('Invalid request context')
+        
+        refresh_token = request.COOKIES.get('refresh_token')
+        if not refresh_token:
+            raise serializers.ValidationError('Refresh token not provided')
+        
+        try:
+            payload = jwt.decode(
+                refresh_token,
+                settings.SECRET_KEY,
+                algorithms=['HS256'],
+            )
+        except jwt.ExpiredSignatureError:
+            raise serializers.ValidationError('Token expired')
+        except jwt.InvalidTokenError:
+            raise serializers.ValidationError('Invalid token')
+        
+        jti = payload.get('jti')
+        if not jti:
+            raise serializers.ValidationError('Invalid token')
+        
+        if redis_client.sismember('token_blacklist', jti):
+            raise serializers.ValidationError('Token revoked')
+        
+        token = RefreshToken.objects.filter(jti=jti).select_related('user').first()
+        if not token:
+            raise serializers.ValidationError('Invalid token')
+        
+        return {
+            'user': token.user
+        }
+        
